36fe4b46383e16c0b6b7c81610f3fc05ede4e9f9
cinyearchan <cinyearchan@gmail.com>
2022-08-23 10:46:15 +0800

    feat: traverser & traverser.spec

--- traverser.spec.ts
+++ traverser.spec.ts
@@ -38,28 +38,28 @@ describe("test ast", () => {
     const visitor: any = {
       Program: {
         enter(node, parent) {
-          callArr.push("program-enter");
+          callArr.push(["program-enter", node.type, ""]);
         },
         exit(node, parent) {
-          callArr.push("program-exit");
+          callArr.push(["program-exit", node.type, ""]);
         },
       },
 
       CallExpression: {
         enter(node, parent) {
-          callArr.push("callExpression-enter");
+          callArr.push(["callExpression-enter", node.type, parent.type]);
         },
         exit(node, parent) {
-          callArr.push("callExpression-exit");
+          callArr.push(["callExpression-exit", node.type, parent.type]);
         },
       },
 
       NumberLiteral: {
         enter(node, parent) {
-          callArr.push("numberLiteral-enter");
+          callArr.push(["numberLiteral-enter", node.type, parent.type]);
         },
         exit(node, parent) {
-          callArr.push("numberLiteral-exit");
+          callArr.push(["numberLiteral-exit", node.type, parent.type]);
         },
       },
     };
@@ -67,18 +67,38 @@ describe("test ast", () => {
     traverser(ast, visitor);
 
     expect(callArr).toEqual([
-      "program-enter",
-      "callExpression-enter",
-      "numberLiteral-enter",
-      "numberLiteral-exit",
-      "callExpression-enter",
-      "numberLiteral-enter",
-      "numberLiteral-exit",
-      "numberLiteral-enter",
-      "numberLiteral-exit",
-      "callExpression-exit",
-      "callExpression-exit",
-      "program-exit",
+      ["program-enter", NodeTypes.Program, ""],
+      ["callExpression-enter", NodeTypes.CallExpression, NodeTypes.Program],
+      [
+        "numberLiteral-enter",
+        NodeTypes.NumberLiteral,
+        NodeTypes.CallExpression,
+      ],
+      ["numberLiteral-exit", NodeTypes.NumberLiteral, NodeTypes.CallExpression],
+      [
+        "callExpression-enter",
+        NodeTypes.CallExpression,
+        NodeTypes.CallExpression,
+      ],
+      [
+        "numberLiteral-enter",
+        NodeTypes.NumberLiteral,
+        NodeTypes.CallExpression,
+      ],
+      ["numberLiteral-exit", NodeTypes.NumberLiteral, NodeTypes.CallExpression],
+      [
+        "numberLiteral-enter",
+        NodeTypes.NumberLiteral,
+        NodeTypes.CallExpression,
+      ],
+      ["numberLiteral-exit", NodeTypes.NumberLiteral, NodeTypes.CallExpression],
+      [
+        "callExpression-exit",
+        NodeTypes.CallExpression,
+        NodeTypes.CallExpression,
+      ],
+      ["callExpression-exit", NodeTypes.CallExpression, NodeTypes.Program],
+      ["program-exit", NodeTypes.Program, ""],
     ]);
   });
 });
--- traverser.ts
+++ traverser.ts
@@ -1,53 +1,58 @@
 import { NodeTypes, RootNode, ChildNode } from "./ast";
 
 interface VisitorOption {
-  enter()
-  exit()
+  enter(node: RootNode | ChildNode, parent: RootNode | ChildNode | undefined);
+  exit(node: RootNode | ChildNode, parent: RootNode | ChildNode | undefined);
 }
 
 interface Visitor {
-  Program?: VisitorOption
-  CallExpression?: VisitorOption
-  NumberLiteral?: VisitorOption
-  StringLiteral?: VisitorOption
+  Program?: VisitorOption;
+  CallExpression?: VisitorOption;
+  NumberLiteral?: VisitorOption;
+  StringLiteral?: VisitorOption;
 }
 
 export function traverser(rootNode: RootNode, visitor: Visitor) {
   // 1. 深度优先遍历
   // 2. visitor
 
-  function traverseArray(array: ChildNode[]) {
+  function traverseArray(
+    array: ChildNode[],
+    parent: RootNode | ChildNode | undefined
+  ) {
     array.forEach((node) => {
-      traverseNode(node);
+      traverseNode(node, parent);
     });
   }
 
-  function traverseNode(node: ChildNode | RootNode) {
-    const visitorObj = visitor[node.type]
+  function traverseNode(
+    node: ChildNode | RootNode,
+    parent?: RootNode | ChildNode
+  ) {
+    const visitorObj = visitor[node.type];
     // enter
     if (visitorObj) {
-      visitorObj.enter()
+      visitorObj.enter(node, parent);
     }
 
     switch (node.type) {
       case NodeTypes.NumberLiteral:
-        console.log("number", node);
+        // console.log("number", node);
+        // traverseNode(node)
         break;
       case NodeTypes.CallExpression:
-        traverseArray(node.params);
+        traverseArray(node.params, node);
         break;
       case NodeTypes.Program:
-        traverseArray(node.body);
+        traverseArray(node.body, node);
         break;
     }
-    
+
     // exit
     if (visitorObj) {
-      visitorObj.exit()
+      visitorObj.exit(node, parent);
     }
   }
-
-  traverseNode(rootNode);
 
   traverseNode(rootNode);
 }
